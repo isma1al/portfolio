a7f7beb44cf23d5caa82eb1ef5a534c5a21d2f98
Isma <ismbek006@mail.ru>
2022-06-10 21:59:40 +0300

    правка багов

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 const emptyMenu = document.querySelector('.empty__menu');
 const burgerMenu = document.querySelector('.burger__menu');
 const burgerBtn = document.querySelector('.burger-menu__btn');
+const title = document.querySelector('.header__title');
 
 burgerBtn.addEventListener('click', function(){
     emptyMenu.classList.toggle('empty__menu--active');
     burgerMenu.classList.toggle('burger__menu--active')
 })
+
+const date = new Date();
+const hours = date.getHours();
+
+if(hours >= 6 && hours < 12){
+    title.innerHTML = 'Good morning';
+} else if(hours >= 12 && hours < 18){
+    title.innerHTML = 'Good afternoon';
+} else if(hours >= 18 && hours < 24) {
+    title.innerHTML = 'Good evening';
+} else {
+    title.innerHTML = 'Good night';
+}
